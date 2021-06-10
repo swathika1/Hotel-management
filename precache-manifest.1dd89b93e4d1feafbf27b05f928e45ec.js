af3dc65551bd9a001d388f8589d252d37d97e8f7
Swathika K <sec19it089@sairamtap.edu.in>
2021-06-10 23:28:33 +0530

    added firebase

---
+++ precache-manifest.1dd89b93e4d1feafbf27b05f928e45ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4e342ea78ac69d0f0c8ac6f46c3784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69fa394f0215b962725",
+    "url": "/static/css/main.a08268fd.chunk.css"
+  },
+  {
+    "revision": "e4fae2d4f8cdc9515da3",
+    "url": "/static/js/2.884234b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.884234b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69fa394f0215b962725",
+    "url": "/static/js/main.5ba18983.chunk.js"
+  },
+  {
+    "revision": "8bd0c811aa8ffb95f252",
+    "url": "/static/js/runtime-main.dabaacfe.js"
+  }
+]);
